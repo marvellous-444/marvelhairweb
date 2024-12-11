c659b4a8b6e7f579495117bfef204d27366c7d70
marvellous-444 <marvellousatare8@gmail.com>
2024-12-11 22:27:38 +0100

    update

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { StrictMode } from "react"
 import React from "react";
 import Home from './Home'
 import './App.css'
-import { BrowserRouter,Route,Routes } from 'react-router-dom'
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 import Services from './Services.jsx'
 import Contact from './Contact.jsx'
 import About from './About.jsx'
@@ -17,13 +17,13 @@ export default  function App(){
     <>
       
       <BrowserRouter>
-        <Routes>
-          <Route path='/' element={<Home/>}/>
-          <Route path='/Services' element={<Services/>}/>
-          <Route path='/Contact' element={<Contact/>}/>
-          <Route path='/AboutUs' element={<About/>}/>
+        <Switch>
+          <Route path='/' component={<Home/>}/>
+          <Route path='/Services' component={<Services/>}/>
+          <Route path='/Contact' component={<Contact/>}/>
+          <Route path='/AboutUs' component={<About/>}/>
           
-        </Routes>
+        </Switch>
       </BrowserRouter>
     
     <div className="body">
